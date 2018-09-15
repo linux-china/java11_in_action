1f1f782d4525c185385b396837c488217652d32c
linux_china <libing.chen@gmail.com>
2018-09-15 11:52:23 -0700

    add more method

--- src/test/java/org/mvnsearch/HttpClientTest.java
+++ src/test/java/org/mvnsearch/HttpClientTest.java
@@ -16,7 +16,17 @@ import java.net.http.HttpResponse;
 public class HttpClientTest {
 
     @Test
-    public void testGet() throws Exception {
+    public void testSyncGet() throws Exception {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://httpbin.org/ip"))
+                .build();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+
+    @Test
+    public void testAsyncGet() {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://httpbin.org/ip"))
@@ -25,5 +35,16 @@ public class HttpClientTest {
                 .thenApply(HttpResponse::body)
                 .thenAccept(System.out::println)
                 .join();
+    }
+
+    @Test
+    public void testHttp2Only() throws Exception {
+        HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:3000"))
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }
